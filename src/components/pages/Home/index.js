ef8e0614b847e201477f4a56122a45d6906c7074
yuvraj-saraswat <114866021+yuvraj-saraswat@users.noreply.github.com>
2023-10-14 19:34:00 +0530

    Add files via upload

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,17 @@
+import Hero from './Hero';
+import WeekSpecials from './WeekSpecials';
+//import Testimonials from './Testimonials';
+import OurStory from './OurStory';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <WeekSpecials />
+      
+      <OurStory />
+    </>
+  );
+};
+
+export default Home;
